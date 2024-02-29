aa3c068a0773e309f75bd5a18adfb04d87afa71e
Jacob Phillips <jifalops@gmail.com>
2024-02-29 16:38:08 -0500

    update devcontainer to use my prebuilt rust image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,8 @@
 # Ensure devcontainer user owns the project directory
-sudo chown -R vscode:vscode /cypher-dto
+sudo chown -R developer:developer $PROJECT_ROOT
 
 # Remember history on the local machine
-ln -s /cypher-dto/.devcontainer/.bash_history ~/.bash_history
+ln -s $PROJECT_ROOT/.devcontainer/.bash_history ~/.bash_history
 
 # Install dotfiles
 gh repo clone dotfiles ~/dotfiles && ~/dotfiles/install.sh
-
-# rustup and cargo bash completion.
-sudo apt-get update -qq && sudo apt-get install -y -qq --no-install-recommends bash-completion \
-  && mkdir -p ~/.local/share/bash-completion/completions \
-  && rustup completions bash > ~/.local/share/bash-completion/completions/rustup \
-  && rustup completions bash cargo > ~/.local/share/bash-completion/completions/cargo
