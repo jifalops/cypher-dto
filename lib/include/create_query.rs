aad2e53308783303bcf63e0c0dcf9230cf8ee4f0
Jacob Phillips <jifalops@gmail.com>
2024-03-01 09:06:39 -0500

    merge changes from 'breaking 0.7' branch

--- lib/include/create_query.rs
+++ lib/include/create_query.rs
@@ -4,15 +4,13 @@
     name: String,
   }
 
-  // Build a query string:
+  let person = Person::new("Alice");
+  let query: neor4s::Query = person.create();
+
+  // Or to build the same query manually:
   let query = format!("CREATE (n:{})", Person::as_query_obj());
   assert_eq!(query, "CREATE (n:Person { name: $name })");
 
-  // Use it in a [neo4rs::Query]:
   let mut query = neo4rs::Query::new(query);
-  let person = Person::new("Alice");
   person.add_values_to_params(query);
-
-  // A shorter way to do the same thing:
-  let query = person.create();
 }
--- lib/include/read_query.rs
+++ lib/include/read_query.rs
@@ -5,15 +5,13 @@
     name: String,
   }
 
-  // Query string:
+  let id = PersonId::new("1234");
+  let query = id.read();
+
+  // Or, to build the query manually:
   let query = format!("MATCH (n:{}) RETURN n", PersonId::as_query_obj());
   assert_eq!(query, "MATCH (n:Person { id: $id }) RETURN n");
 
-  // Using [neo4rs::Query] manually:
   let mut query = neo4rs::Query::new(query);
-  let id = PersonId::new("1234");
   id.add_values_to_params(query);
-
-  // A shorter way to do the same thing:
-  let query = id.read();
 }
